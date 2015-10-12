fc4b023fa69c61dcca4ffef5d1db838e4af2a436
Alfredo Barron <alfredobarronc@gmail.com>
2015-10-12 18:38:18 -0500

    Deploy file component v1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+
+var DEST = 'dist/';
+
+gulp.task('minify-js', function() {
+  return gulp.src('file/file.js')
+    .pipe(gulp.dest(DEST))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(DEST));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('file/file.css')
+    .pipe(gulp.dest(DEST))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest(DEST));
+});
+
+gulp.task('default', ['minify-js', 'minify-css']);
